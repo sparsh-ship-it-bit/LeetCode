c83af7838304630791dcba6d12e5f42b3131927d
sparsh-ship-it-bit <singhalsparsh34@gmail.com>
2026-09-16 20:20:30 +0530

    Time: 4 ms (24.53%) | Memory: 76.6 MB (38.11%) - LeetSync

--- 2767-maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.cpp
+++ 2767-maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.cpp
@@ -3,16 +3,12 @@ public:
     int maximizeSum(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
 
+        int maxNum = nums[nums.size() - 1];
         int maxSum = 0;
 
         while(k > 0) {
-            int n = nums.size();
-            int maxNum = 0;
-
-            maxNum = max(maxNum, nums[n-1]);
             maxSum += maxNum;
-            nums[n-1] = nums[n-1] + 1;
-
+            maxNum++;
             k--;
         }
 
