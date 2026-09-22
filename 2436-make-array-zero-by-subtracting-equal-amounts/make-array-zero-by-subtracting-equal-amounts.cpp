57c87a6469f67621f385b300da2dd2da739f1bb9
sparsh-ship-it-bit <singhalsparsh34@gmail.com>
2026-09-22 22:07:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (25.77%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int> s;
+
+        for (int x : nums) {
+            if (x != 0)
+                s.insert(x);
+        }
+
+        return s.size();
+    }
+};
